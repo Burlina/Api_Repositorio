83969884cd4b738a813c58b65516a910f5876a26
Burlina <nathalia.marques@sptech.school>
2022-05-25 19:03:28 -0300

    ATUALIZAÇÃO BANCO

--- src/database/ACQUATEC.sql
+++ src/database/ACQUATEC.sql
@@ -1,3 +1,8 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/* para workbench - local - desenvolvimento */
 CREATE DATABASE acquatec;
 
 USE acquatec;
@@ -6,21 +11,51 @@ CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
+    cpf VARCHAR(50),
 	senha VARCHAR(50)
 );
 
 CREATE TABLE aviso (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	titulo VARCHAR(100),
-	descricao VARCHAR(150),
+    descricao VARCHAR(150),
 	fk_usuario INT,
 	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
+); 
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
+CREATE TABLE medida (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300)
+	temperatura DOUBLE,
+	umidade DOUBLE,
+	momento DATETIME,
+	fk_aquario INT
 );
 
-select * from usuario;
+
+
+
+/* para sql server - remoto - produção
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50),
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	titulo VARCHAR(100),
+    descricao VARCHAR(150),
+	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
+); 
+
+CREATE TABLE medida (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	temperatura DOUBLE,
+	umidade DOUBLE,
+	momento DATETIME,
+	fk_aquario INT
+);
+
+*/
--- src/database/config.js
+++ src/database/config.js
@@ -21,7 +21,7 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     user: "Burlina",
-    database: "acquatec",
+    database: "ACQUATEC",
     password: "Urubu100.",
 };
 
