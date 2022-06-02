fa6dd484fa41f9d7291f49daf699ed7010c2e9a9
Burlina <nathalia.marques@sptech.school>
2022-06-02 17:42:57 -0300

    atualização database

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -3,13 +3,14 @@ var database = require("../database/config");
 function votar(IdFadas, limites_linhas){
     instrucaoSql = `INSERT INTO 
     votacao (FkFada) 
-    VALUES (${IdFadas});`;
+    VALUES (${IdFadas})`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 function buscarUltimasMedidas(IdFadas, limite_linhas) {
-    instrucaoSql = `select nome, count(fkfada) as id from fadas
+    instrucaoSql = `select nome, count(fkfada) as id 
+    from fadas
     join votacao
     on fkfada = idfadas
     group by fkFada
